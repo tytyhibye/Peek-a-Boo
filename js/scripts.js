aaa5f924e078c4e7dcc2af0652ba8f60d363330a
Tyler Bates <Fondlytyler@gmail.com>
2020-03-24 10:09:46 -0700

    bug fix on clickable text

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  $("p").click(function(){
-    $(".cat-showing").toggle();
-    $(".cat-hidden").toggle();
+  $(".clickable").click(function(){
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
   });
 });
