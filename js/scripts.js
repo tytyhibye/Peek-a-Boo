c0bc505bfe7f6b3ffd7da0d8d41b9b36bf212415
Tyler Bates <Fondlytyler@gmail.com>
2020-03-24 10:14:48 -0700

    Add fade to the toggle showing

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-  $(".clickable").click(function(){
-    $("#initially-showing").toggle();
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#initially-showing").fadeToggle();
     $("#initially-hidden").toggle();
   });
 });
